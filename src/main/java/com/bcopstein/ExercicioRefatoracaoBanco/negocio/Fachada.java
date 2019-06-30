bb90e9c960a1010b7633ef06120460febec027c0
Erick Pintor <erickpintor@gmail.com>
2019-06-30 19:03:11 -0300

    corrige progemas de merge

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/negocio/Fachada.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/negocio/Fachada.java
@@ -1,20 +1,18 @@
 package com.bcopstein.ExercicioRefatoracaoBanco.negocio;
 
-import java.util.List;
-import java.util.GregorianCalendar;
-
 import com.bcopstein.ExercicioRefatoracaoBanco.Persistencia;
+
+import java.util.GregorianCalendar;
+import java.util.List;
 
 public class Fachada {
     private ContaController conta;
     private OperacaoController operacoes;
-    private Persistencia persistencia;
     private GregorianCalendar gc;
 
     public Fachada() {
-        this.persistencia = new Persistencia();
-        this.operacoes = new OperacaoController(this.persistencia.loadOperacoes());
-        this.conta = new ContaController(this.persistencia.loadContas());
+        this.operacoes = new OperacaoController(Persistencia.getInstance().loadOperacoes());
+        this.conta = new ContaController(Persistencia.getInstance().loadContas());
         this.gc = new GregorianCalendar();
     }
 
@@ -33,8 +31,8 @@ public class Fachada {
         contaAtual = this.conta.getConta(numeroConta);
         contaAtual.deposito(valor);
         this.operacoes.AddOperacao(gc.get(GregorianCalendar.DAY_OF_MONTH), gc.get(GregorianCalendar.MONTH) + 1,
-                gc.get(GregorianCalendar.YEAR), gc.get(GregorianCalendar.HOUR), gc.get(GregorianCalendar.MINUTE),
-                gc.get(GregorianCalendar.SECOND), contaAtual.getNumero(), contaAtual.getStatus(), valor, 0);
+            gc.get(GregorianCalendar.YEAR), gc.get(GregorianCalendar.HOUR), gc.get(GregorianCalendar.MINUTE),
+            gc.get(GregorianCalendar.SECOND), contaAtual.getNumero(), contaAtual.getStatus(), valor, 0);
         return true;
     }
 
@@ -50,15 +48,15 @@ public class Fachada {
 
         contaAtual = this.conta.getConta(numeroConta);
         double valorDiario = this.operacoes.valorDiarioDebito(numeroConta, gc.get(GregorianCalendar.DAY_OF_MONTH),
-                gc.get(GregorianCalendar.MONTH) + 1, gc.get(GregorianCalendar.YEAR));
-                
+            gc.get(GregorianCalendar.MONTH) + 1, gc.get(GregorianCalendar.YEAR));
+
         if (contaAtual.getLimRetiradaDiaria() < valorDiario) {
             return false;
         } else {
             contaAtual.retirada(valor);
             this.operacoes.AddOperacao(gc.get(GregorianCalendar.DAY_OF_MONTH), gc.get(GregorianCalendar.MONTH) + 1,
-                    gc.get(GregorianCalendar.YEAR), gc.get(GregorianCalendar.HOUR), gc.get(GregorianCalendar.MINUTE),
-                    gc.get(GregorianCalendar.SECOND), contaAtual.getNumero(), contaAtual.getStatus(), valor, 1);
+                gc.get(GregorianCalendar.YEAR), gc.get(GregorianCalendar.HOUR), gc.get(GregorianCalendar.MINUTE),
+                gc.get(GregorianCalendar.SECOND), contaAtual.getNumero(), contaAtual.getStatus(), valor, 1);
             return true;
         }
 
@@ -78,10 +76,10 @@ public class Fachada {
     }
 
     public void saveContas() {
-        this.persistencia.saveContas(conta.getContas().values());
+        Persistencia.getInstance().saveContas(conta.getContas().values());
     }
 
     public void saveOperacoes() {
-        this.persistencia.saveOperacoes(operacoes.getOperacoes());
+        Persistencia.getInstance().saveOperacoes(operacoes.getOperacoes());
     }
 }
